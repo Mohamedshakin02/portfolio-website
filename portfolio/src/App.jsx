e6f846b610a8679124941768435ba660441c2b8e
Mohamed Shakin <shakincc02@gmail.com>
2025-08-19 17:18:41 +0400

    Complete intro section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,40 +5,38 @@ import Photos from "../pages/Photos";
 import Arts from "../pages/Arts";
 import Home from "./components/Home";
 import Background from "./components/background";
-import Header from "./components/header";
+import Header from "./components/Header";
 
 function Layout({ children }) {
   const location = useLocation();
   const isHome = location.pathname === "/";
 
   return (
-    <div style={{ position: "relative" }}>
+    <div style={{ position: "relative"}}>
       {/* Background always clickable behind */}
       <div
         style={{
           position: "absolute",
-          top: 0, left: 0, right: 0, bottom: 0,
-          zIndex: 0
+          top: 0, left: 0, right: 0, bottom: 0, zIndex: 0
         }}
       >
         <Background />
       </div>
 
       {/* Wrap foreground content in a "clickable only on content" layer */}
-      <div style={{ position: "relative", zIndex: 0, pointerEvents: "none" }}>
+      <div style={{ position: "relative", zIndex: 1, pointerEvents: "none" }}>
         {isHome && (
           <div style={{ pointerEvents: "auto" }}>
-            <Header />
+            <Header/>
           </div>
         )}
 
         <main
-          className="container-lg mx-2 mx-lg-auto"
+          className="container-fluid p-0"
           style={{
             position: "relative",
             top: 150,
-            pointerEvents: "auto",
-            zIndex: "-1",   
+            pointerEvents: "none",
           }}
         >
           {children}
--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -41,7 +41,7 @@ function Header() {
   };
 
   return (
-    <header className="position-absolute top-25 start-50 translate-middle container-lg my-4 mx-2 mx-lg-auto">
+    <header className="position-absolute top-10 start-50 translate-middle container-lg my-4">
       <nav className="navbar navbar-expand-md bg-body-tertiary rounded-pill w-100  px-3 container-lg position-relative z-3">
         <div className="container-fluid d-flex align-items-center">
           {/* Small screen title */}
