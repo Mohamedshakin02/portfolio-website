c04070dd90c8daf2d6c43576d22cce5ccf54cf1a
Mohamed Shakin <shakincc02@gmail.com>
2025-08-24 00:09:26 +0400

    complete about page

--- portfolio/pages/AboutPage.jsx
+++ portfolio/pages/AboutPage.jsx
@@ -2,13 +2,25 @@ import React from 'react'
 import About from '../src/components/About'
 import "../stylesheet/about_page.css";
 import Education from '../src/components/Education';
+import Skills from '../src/components/Skills'
+import Footer from '../src/components/Footer';
 
 function AboutPage() {
     return (
-        <div className="about-page-wrapper">
-            <About />
-            <Education/>
-        </div>
+        <>
+            <div className="about-section">
+                <About />
+            </div>
+            <Education />
+            <div className="skills-section">
+                <Skills/>
+            </div>
+            <div className="footer-section">
+                <Footer/>
+            </div>    
+            
+        </>
+
     )
 }
 
