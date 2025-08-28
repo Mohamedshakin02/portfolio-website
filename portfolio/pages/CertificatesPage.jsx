22b4f5ba2129318c4ddc1f363c2b48aa1defccf1
Mohamed Shakin <shakincc02@gmail.com>
2025-08-28 12:08:55 +0400

    complete certificates page

--- portfolio/pages/CertificatesPage.jsx
+++ portfolio/pages/CertificatesPage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import AcademicCerificates from '../src/components/AcademicCerificates'
 import CourseCertificates from '../src/components/CourseCertificates'
+import Footer from '../src/components/Footer';
 
 function CertificatesPage() {
   return (
     <>
       <CourseCertificates />
       <AcademicCerificates />
-
+      <div className="certificates-footer">
+        <Footer />
+      </div>
     </>
   )
 }
