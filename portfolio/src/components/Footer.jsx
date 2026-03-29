735f1eab16ae4e0d48ff20f243348209ff479a58
Mohamed Shakin <shakincc02@gmail.com>
2026-03-29 17:18:19 +0400

    adjusted the font-weight

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -16,19 +16,19 @@ function Footer() {
                         <div className='links'>
                             <h3 className='h1'>Links</h3>
                             <div className='link'>
-                                <a href="https://www.linkedin.com/in/mohamed-shakin/" target="_blank" className='text-decoration-none text-body text-dark fs-4 fw-medium' title='LinkedIn Link'>LinkedIn</a>
+                                <a href="https://www.linkedin.com/in/mohamed-shakin/" target="_blank" className='text-decoration-none text-body text-dark fs-4 fw-normal' title='LinkedIn Link'>LinkedIn</a>
                             </div>
 
                             <div className='link'>
-                                <a href="https://github.com/Mohamedshakin02" target="_blank" className='text-decoration-none text-body text-dark fs-4 fw-medium' title='Github Link'>Github</a>
+                                <a href="https://github.com/Mohamedshakin02" target="_blank" className='text-decoration-none text-body text-dark fs-4 fw-normal' title='Github Link'>Github</a>
                             </div>
 
                             <div className='link'>
-                                <a href="https://mohamed-shakin.itch.io/" target="_blank" className='text-decoration-none text-body text-dark fs-4 fw-medium' title='itch.io Link'>itch.io</a>
+                                <a href="https://mohamed-shakin.itch.io/" target="_blank" className='text-decoration-none text-body text-dark fs-4 fw-normal' title='itch.io Link'>itch.io</a>
                             </div>
 
                             <div className='link'>
-                                <a href="https://www.youtube.com/@mohamedshakin5397" target="_blank" className='text-decoration-none text-body text-dark fs-4 fw-medium' title='Youtube Link'>Youtube</a>
+                                <a href="https://www.youtube.com/@mohamedshakin5397" target="_blank" className='text-decoration-none text-body text-dark fs-4 fw-normal' title='Youtube Link'>Youtube</a>
                             </div>
                         </div>
 
@@ -37,14 +37,14 @@ function Footer() {
 
                             <a href="mailto:m.shakin2005@gmail.com"
                                 target="_blank"
-                                className="d-block text-decoration-none text-dark fs-4 fw-medium"
+                                className="d-block text-decoration-none text-dark fs-4 fw-normal"
                                 title='Email ID'>
                                 m.shakin<span className="number fs-5">2005</span>@gmail.com
                             </a>
 
                             <a href="tel:+971504385645"
                                 target="_blank"
-                                className="number d-block mt-2 text-decoration-none text-dark fs-5 fw-medium"
+                                className="number d-block mt-2 text-decoration-none text-dark fs-5 fw-normal"
                                 title='Mobile Number'>
                                 +971504385645
                             </a>
