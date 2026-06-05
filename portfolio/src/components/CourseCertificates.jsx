257ab3f2b37905d5292c75a40c5a8b17b7439428
Mohamed Shakin <shakincc02@gmail.com>
2026-06-05 10:46:53 +0400

    added certificate

--- portfolio/src/components/CourseCertificates.jsx
+++ portfolio/src/components/CourseCertificates.jsx
@@ -29,6 +29,7 @@ import CertificateImg19 from '../assets/certificates/NextJS.png';
 import CertificateImg20 from '../assets/certificates/Introduction to Cybersecurity.png';
 import CertificateImg21 from '../assets/certificates/Learning Cyber Incident Response and Digital Forensics.png';
 import CertificateImg22 from '../assets/certificates/Autopsy for Digital Forensics.png';
+import CertificateImg23 from '../assets/certificates/Power BI Desktop.png';
 
 function CourseCertificates() {
     const [certificates] = useState([
@@ -54,6 +55,7 @@ function CourseCertificates() {
         { title: "Introduction to Cybersecurity Certificate", image: CertificateImg20 },
         { title: "Learning Cyber Incident Response and Digital Forensics Certificate", image: CertificateImg21 },
         { title: "Autopsy for Digital Forensics Certificate", image: CertificateImg22 },
+        { title: "Power BI Desktop Certificate", image: CertificateImg23 },
     ]);
 
     const swiperRef = useRef(null);
